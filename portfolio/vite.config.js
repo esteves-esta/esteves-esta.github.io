255ef6e039856fb52cc00b58a762a9ed2ce54511
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2023-12-16 11:36:18 -0300

    update workflow and !vite.config base!

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const config = {
+    base: mode === "production" ? "/esteves-esta.github.io/" : "",
+    plugins: [react()],
+  };
+  return config;
+});
