48089781df71acd6aeb6b24ddb4afde0a81366b4
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2023-12-16 12:24:47 -0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const config = {
-    base: mode === "production" ? "/esteves-esta.github.io/" : "",
+    base: mode === "production" ? "/" : "",
     plugins: [react()],
   };
   return config;
