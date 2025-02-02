84c64bed99d38996598adeee0d52fb34bfebc1ff
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2025-02-02 13:35:39 -0300

    links and colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,27 @@
+// YEARS WORKING
 const current_year = new Date().getFullYear();
-document.querySelector('#anos-trabalho').innerHTML = current_year - 2019;
+document.querySelector("#anos-trabalho").innerHTML = current_year - 2019;
+
+// TIME FUNCTION
+function getTime() {
+  var options = { hour: "2-digit", minute: "2-digit" };
+  const currentHour = new Date().toLocaleTimeString("pt-BR", options);
+
+  const dayPeriod = new Date().toLocaleTimeString();
+  let greeting = "Bom dia";
+  const split = dayPeriod.split(":");
+  if (dayPeriod.includes("PM")) {
+    if (split[0] < 6) greeting = "Boa tarde";
+    else greeting = "Boa noite";
+  } else {
+    if (split[0] < 5) greeting = "Boa noite";
+  }
+
+  document.querySelector("#time-now").innerHTML = currentHour;
+  document.querySelector("#greeting").innerHTML = greeting;
+}
+getTime();
+
+window.setInterval(() => {
+  getTime();
+}, 3 * 10000);
