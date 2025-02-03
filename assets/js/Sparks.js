08d0ce1b5acbbeea7d1013cb144507d0e95723f5
Fernanda Esteves <fernanda.esteves@clear.sale>
2025-02-03 15:47:16 -0300

    adicionando footer

---
+++ assets/js/Sparks.js
@@ -0,0 +1,26 @@
+const SparkStyle = `
+span {
+  color: var(--color-red);
+  font-style: italic;
+}
+`;
+// https://creativecommons.org/share-your-work/cclicenses/
+class Sparks extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+
+    const template = document.createElement("custom-span");
+    this.shadow.appendChild(template);
+
+    this.shadow.innerHTML = `
+    <style>${SparkStyle}</style>
+    <span>
+        &#127878; <slot name="username"></slot> &#127879;
+    </span>`;
+  }
+
+
+}
+
+customElements.define("spark-text", Sparks);
